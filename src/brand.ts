2efd015d7bbafb66695d1010ed0f5ac8e6e67bfa
drewzhu <drew.zhu@radarbrokers.com>
2026-07-28 19:40:58 +1000

    Bust cached property styling logo

--- src/brand.ts
+++ src/brand.ts
@@ -1,1 +1,1 @@
-export const brandLogo = `${import.meta.env.BASE_URL}vale-co-property-styling-logo.png`
+export const brandLogo = `${import.meta.env.BASE_URL}vale-co-property-styling-logo.png?v=20260728-property-styling`
